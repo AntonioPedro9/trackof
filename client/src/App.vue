f1cf9d4b578df451dab96e93b97d815099333fd6
Antônio Pedro <antoniopedro.rs9@gmail.com>
2021-11-10 15:47:36 -0300

    Change app colors

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,7 +8,7 @@
         :name="board.name"
         :id="board.id"
       />
-      <button @click="createBoard()" class="fab theme-pink">
+      <button @click="createBoard()" class="fab theme-deep-orange">
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0z" fill="none" /><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" /></svg>
       </button>
     </main>
